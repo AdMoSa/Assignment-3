e800163e2d836e42a4d2214ea82b0f7850ed0942
AdMoSa <31254548+AdMoSa@users.noreply.github.com>
2017-09-05 15:37:21 +0530

    Create Ques 1.cpp

---
+++ Ques 1.cpp
@@ -0,0 +1,18 @@
+# Assignment-3
+#include <iostream>
+using namespace std;
+
+main()
+{
+ float x,y,max;
+ cout << "Enter 1st number: ";
+ cin >> x;
+ cout << "Enter 2nd number: ";
+ cin >> y;
+ if(x>=y)
+    max=x;
+ else
+    max=y;
+ cout << "Maximum is: "<< max;
+}
+
