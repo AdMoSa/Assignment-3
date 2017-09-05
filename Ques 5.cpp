f7a23f5c87e7c54be7984ceab45940d757671596
AdMoSa <31254548+AdMoSa@users.noreply.github.com>
2017-09-05 16:12:30 +0530

    Create Ques 5.cpp

---
+++ Ques 5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+ int a;
+ cout << "Enter your number: ";
+ cin >> a;
+ if(a%2==0)
+    cout << "Its an even number";
+ else
+    cout << "Its an odd number";
+}
