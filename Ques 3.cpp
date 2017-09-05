7f53e77f83c39588addd6889a0c660a11ac08ac7
AdMoSa <31254548+AdMoSa@users.noreply.github.com>
2017-09-05 15:50:19 +0530

    Create Ques 3.cpp

---
+++ Ques 3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+ int a;
+ cout << "Enter number: ";
+ cin >> a;
+ if(a>0)
+    cout << "a is positive";
+ else if(a<0)
+    cout << "a is negative";
+ else
+    cout << "a is zero";
+}
+
+ 
