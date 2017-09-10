07a1cea5266c4cdbb12c92098bc20292f466ad1e
AdMoSa <31254548+AdMoSa@users.noreply.github.com>
2017-09-10 12:28:58 +0530

    Update Ques 7.cpp

--- Ques 7.cpp
+++ Ques 7.cpp
@@ -6,7 +6,7 @@ int main()
 	char a;
     cout << "Enter the character: ";
     cin >> a;
-	if ((a >= 65 && a <= 90) ^ (a >= 97 && a <= 122))
+	if ((a >= 65 && a <= 90) || (a >= 97 && a <= 122))
 		cout << "Its a character.";
 	else
 		cout << "Its not a character.";
