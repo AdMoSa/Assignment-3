ddee5beb152c0c38a0135defd7a8feac0900dd6b
AdMoSa <31254548+AdMoSa@users.noreply.github.com>
2017-09-15 03:13:45 +0530

    Create Ques 30.cpp

---
+++ Ques 30.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cout<<"Enter any number : ";
+  cin>>n;
+  
+  for (int x=1;x<=10;x++)
+  { 
+    cout << n << " * " << x << " = " << (n*x) << endl;
+  }
+  
+  return 0;
+}
