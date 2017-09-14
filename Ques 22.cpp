ee4ad2914ef1b92f8f84b688580e0cef9ab95009
AdMoSa <31254548+AdMoSa@users.noreply.github.com>
2017-09-15 02:36:16 +0530

    Create Ques 22.cpp

---
+++ Ques 22.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int n,x;
+  x=1;
+  cout<<"Enter the nth term : ";
+  cin>>n;
+  
+ while(x<=n)
+ {
+    cout<<x;
+    cout<<endl;
+    x++;
+ }
+  return 0;
+}
