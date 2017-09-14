ffd33caa600a1bcf7fbe86c156684ab2c21edc4a
AdMoSa <31254548+AdMoSa@users.noreply.github.com>
2017-09-15 03:02:26 +0530

    Create Ques 27.cpp

---
+++ Ques 27.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n,sum,a;
+  a=1;
+  sum=0;
+  
+  cout<<"Enter nth term : ";
+  cin>>n;
+  
+  while(a<=n)
+  {
+    sum=sum+a;
+    a++;
+  }
+  
+  cout<<"The sum of 1 to n : "<<sum;
+  
+  return 0;
+}
